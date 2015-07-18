9ee6d390640e428c510737d1b309824d2f5464f9
ongleongoh <onglo1@yahoo.com.sg>
2015-07-18 11:11:23 +0800

    initial commit

---
+++ modelBasedPrediction.R
@@ -0,0 +1,16 @@
+library(ggplot2)
+library(caret)
+names(iris)
+table(iris$Species)
+inTrain <- createDataPartition(y=iris$Species, p=0.7, list=FALSE)
+training <- iris[inTrain,]
+testing <- iris[-inTrain,]
+dim(training)
+dim(testing)
+modlda <- train(Species ~., data=training, method="lda")
+modnb <- train(Species ~., data=training, method="nb")
+plda <- predict(modlda, testing)
+pnb  <- predict(modnb, testing)
+table(plda,pnb)
+equalPredictions <- (plda==pnb)
+qplot(Petal.Width, Sepal.Width, colour=equalPredictions, data=testing)
